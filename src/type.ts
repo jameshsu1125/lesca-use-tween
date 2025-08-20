8209bc316cabcda2c51c1b2909a3c2f06597dbd9
James Hsu <jameshsu1125@gmail.com>
2025-08-20 11:49:41 +0800

    Update package dependencies and modify ProviderProps to allow tweenStyle to be false

--- src/type.ts
+++ src/type.ts
@@ -35,7 +35,7 @@ export interface CSS extends Omit<CSSProperties, 'rotate'> {
 export interface ProviderProps {
   children: ReactElement;
   initStyle: CSS;
-  tweenStyle?: CSS;
+  tweenStyle?: CSS | false;
   options?: Partial<Setting>;
 }
 
